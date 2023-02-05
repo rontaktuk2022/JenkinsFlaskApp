6f040d9329720d8cde130d92c90cbfc2ba2b1bbc
OrAbitbul <117725382+OrAbitbul@users.noreply.github.com>
2023-02-05 12:10:22 +0000

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -6,4 +6,5 @@ def hello_world():
     return 'Hello, World! v1'
 
 if __name__ == "__main__":
-   app.run()
+   port = int(os.environ.get("PORT", 5000))
+   app.run(debug=True, host='0.0.0.0', port=port)
